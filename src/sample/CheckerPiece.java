5b1c427910294d0f99ccff3e63ead36ae978baa5
BT_1N3_34 <pyu6253@bths.edu>
2019-02-01 09:24:48 -0500

    base

--- src/sample/CheckerPiece.java
+++ src/sample/CheckerPiece.java
@@ -1,15 +1,25 @@
 package sample;
 
 public class CheckerPiece {
+
+    private String team;
+    private int x;
+    private int y;
+
+    public CheckerPiece(String team, int x, int y)
+    {
+        this.team = team;
+        this.x = x;
+        this.y = y;
+    }
     public boolean isLegal(String team)
     {
 
-        return false;
     }
     public void move()
     {
         isLegal("red");
-        capture("red");
+
     }
     public boolean capture(String team)
     {
